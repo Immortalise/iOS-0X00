987ecfe6f7bc45eef113b2762c3a966a04a247c4
Immortalise <31989262+Immortalise@users.noreply.github.com>
2017-10-25 21:48:23 +0800

    Animal.m

---
+++ Animal.m
@@ -0,0 +1,20 @@
+//
+//  Animal.m
+//  宠物世界
+//
+//  Created by 朱凯捷 on 2017/10/25.
+//  Copyright © 2017年 朱凯捷. All rights reserved.
+//
+
+#import "Animal.h"
+
+@implementation Animal
+
+@synthesize _name;
+@synthesize owner;
+
+-(void)beTouched
+{
+}
+
+@end
